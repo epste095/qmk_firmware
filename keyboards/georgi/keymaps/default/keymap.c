40efa49900e7acdb23bae814feb673d2b4f3dc93
epste095 <62880821+epste095@users.noreply.github.com>
2020-05-28 19:54:12 -0700

    Update keymap.c

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -232,8 +232,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Gaming layer with Numpad, Very limited
     [GAMING] = LAYOUT_georgi(
         LT(GAMING_2, KC_ENT), KC_Q,    KC_Q,    KC_EQL,    KC_E,    KC_T,                       KC_TRNS,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
-        TO(STENO_LAYER), KC_SCLN,    KC_A,    KC_S,    KC_D,    KC_G,                       KC_B,    KC_K,    KC_SPC,    KC_DOWN,    KC_W, KC_DQUO,
-                                   KC_LALT, KC_SPC, KC_LSFT,       KC_SCLN,  KC_J, KC_MS_BTN1
+        KC_TRNS, KC_SCLN,    KC_A,    KC_S,    KC_D,    KC_G,                       KC_B,    KC_K,    KC_SPC,    KC_DOWN,    KC_W, KC_DQUO,
+                                   KC_LALT, KC_C, KC_N,       KC_M,  KC_J, KC_MS_BTN1
     ),
 
     [GAMING_2] = LAYOUT_georgi(
