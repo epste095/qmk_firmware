7cab6d842663838f0fd5e9baf930b14ef50330f7
epste095 <62880821+epste095@users.noreply.github.com>
2020-06-03 11:18:18 -0700

    Update keymap.c

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -237,9 +237,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [GAMING_2] = LAYOUT_georgi(
-        KC_LSFT, KC_4,    KC_1,    KC_W,    KC_2,    KC_3,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_LCTL, KC_5,    KC_A,    KC_S,    KC_D,    KC_6,          KC_N,    KC_U,    KC_I,   KC_O,   KC_P, KC_RSFT,
-                                   KC_8, KC_0,  KC_9,        KC_TRNS,  KC_J, TO(STENO_LAYER)
+        KC_H, KC_4,    KC_1,    KC_W,    KC_2,    KC_3,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+        KC_G, KC_5,    KC_A,    KC_S,    KC_D,    KC_6,          KC_N,    KC_U,    KC_I,   KC_O,   KC_P, V,
+                                   KC_8, KC_0,  KC_LSFT,        KC_TRNS,  KC_J, TO(STENO_LAYER)
     )
 };
 
