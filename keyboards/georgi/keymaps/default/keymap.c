8531d3d4e3c6332e3260e7bf3f3e9a9394d96fc0
epste095 <62880821+epste095@users.noreply.github.com>
2020-06-08 21:49:16 -0700

    Update keymap.c

--- keyboards/georgi/keymaps/default/keymap.c
+++ keyboards/georgi/keymaps/default/keymap.c
@@ -240,13 +240,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [GAMING_2] = LAYOUT_georgi(
         KC_H, KC_4,    KC_1,    KC_W,    KC_2,    KC_3,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_G, KC_5,    KC_A,    KC_S,    KC_D,    KC_6,          KC_N,    KC_U,    KC_I,   KC_O,   KC_P, KC_V,
-                                   KC_8, KC_0,  KC_LSFT,        KC_TRNS,  KC_J, TO(STENO_LAYER)
+                                   KC_8, KC_0,  KC_LSFT,        KC_TRNS,  KC_J, TG(GAMING_3)
     ),
         
     [GAMING_3] = LAYOUT_georgi(
         KC_Q, KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,          KC_U,    KC_I,    KC_O,    KC_P,    KC_Z,    KC_X,
         KC_A, KC_S,    KC_D,    KC_F,    KC_G,    KC_H,          KC_J,    KC_K,    KC_L,   KC_C,   KC_V, KC_B,
-                                   KC_BSPC, KC_N,  KC_LSFT,        KC_SPC,  KC_M, TO(STENO_LAYER)
+                                   KC_BSPC, KC_N,  KC_LSFT,        KC_SPC,  KC_M, TG(GAMING_2)
     )
 };
 
