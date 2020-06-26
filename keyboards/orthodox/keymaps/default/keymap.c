cdde1efb7c927fbc0b5bfdf0eeeef25f3c739e0b
epste095 <62880821+epste095@users.noreply.github.com>
2020-06-25 18:51:45 -0700

    Update keymap.c

--- keyboards/orthodox/keymaps/default/keymap.c
+++ keyboards/orthodox/keymaps/default/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_GAMING] = LAYOUT(\
     KC_TAB,   KC_QUOT, KC_Q, KC_EQL, KC_E,     KC_Y,                                                      KC_F,    KC_SCLN,    KC_RIGHT,    KC_R,    KC_E,    TO(_COLEMAK), \
     KC_ESC,   KC_SCLN,    KC_A,    KC_S,   KC_D,     KC_I, KC_UP,    KC_DOWN,        KC_I, KC_O,          KC_D,    KC_K,    KC_SPACE,    KC_L,    KC_W,    KC_N, \
-    KC_LCTL,  KC_X, KC_Q,    KC_J,   KC_K,     KC_X,      LOWER, KC_C, KC_N,        KC_U,  KC_J, KC_P,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_LGUI \
+    KC_LCTL,  KC_X, KC_Q,    KC_J,   KC_K,     KC_X,      KC_V, KC_C, KC_N,        KC_U,  KC_J, KC_P,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_LGUI \
   ),
 
   [_LOWER] = LAYOUT( \
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            _______, _______,          _______, _______,          KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______,          _______, _______, _______, KC_F12,  KC_PGUP, KC_COMM, KC_DOT,  KC_PGDN, _______ \
+    RESET, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, _______,          _______, _______, _______, KC_F12,  KC_PGUP, KC_COMM, KC_DOT,  KC_PGDN, _______ \
   ),
 
   [_ADJUST] =  LAYOUT( \
